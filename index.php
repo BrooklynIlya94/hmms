ccb339a18ea5c4e8599905efa8846a0c9a4b8a0b
BrooklynIlya94 <ilya1994kaloshin@gmail.com>
2021-05-11 12:52:08 +0300

    Update

--- index.php
+++ index.php
@@ -3,15 +3,13 @@
 
 
 <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-T7RMMHG6GY"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-T7RMMHG6GY');
-    </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WWPTX79');</script>
+    <!-- End Google Tag Manager -->
     
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Главная</title>
@@ -43,7 +41,15 @@
     </style>
 </head>
 
+
+
+
+
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WWPTX79"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <!--::header part start::-->
     <header class="main_menu home_menu">
         <div class="container">
